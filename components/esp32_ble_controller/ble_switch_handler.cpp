0f1193c9d8897b47bbed4864431caefe56b456c5
simdollaro <59896693+simdollaro@users.noreply.github.com>
2025-08-08 13:50:10 +0200

    Update ble_switch_handler.cpp

--- components/esp32_ble_controller/ble_switch_handler.cpp
+++ components/esp32_ble_controller/ble_switch_handler.cpp
@@ -19,7 +19,7 @@ void BLESwitchHandler::on_characteristic_written() {
   if (value.length() == 1) {
     uint8_t on = value[0];
     ESP_LOGD(TAG, "Switch chracteristic written: %d", on);
-    if (value=1)
+    if (value == "1")
       get_component()->turn_on();
     else
       get_component()->turn_off();
